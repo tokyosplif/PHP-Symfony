327efab861cf192ff944a1ab7f5de52979bf04d8
oleg <tokyospliff3@gmail.com>
2025-02-10 17:39:16 +0200

    update service auth

--- src/Service/AuthService.php
+++ src/Service/AuthService.php
@@ -3,24 +3,25 @@
 namespace App\Service;
 
 use App\Entity\User;
+use App\Repository\AuthRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 class AuthService
 {
-    private EntityManagerInterface $entityManager;
+    private AuthRepository $authRepository;
     private LoggerInterface $logger;
 
-    public function __construct(EntityManagerInterface $entityManager, LoggerInterface $logger)
+    public function __construct(AuthRepository $authRepository, LoggerInterface $logger)
     {
-        $this->entityManager = $entityManager;
+        $this->authRepository = $authRepository;
         $this->logger = $logger;
     }
 
     public function registerUser(string $username, string $password): bool
     {
-        $existingUser = $this->entityManager->getRepository(User::class)->findOneBy(['username' => $username]);
+        $existingUser = $this->authRepository->findByUsername($username);
         if ($existingUser) {
             return false;
         }
@@ -29,15 +30,14 @@ class AuthService
         $user->setUsername($username);
         $user->setPassword(password_hash($password, PASSWORD_BCRYPT));
 
-        $this->entityManager->persist($user);
-        $this->entityManager->flush();
+        $this->authRepository->save($user, true);
 
         return true;
     }
 
     public function authenticateUser(string $username, string $password): ?User
     {
-        $user = $this->entityManager->getRepository(User::class)->findOneBy(['username' => $username]);
+        $user = $this->authRepository->findByUsername($username);
 
         if ($user && password_verify($password, $user->getPassword())) {
             return $user;
@@ -48,12 +48,12 @@ class AuthService
 
     public function findByUsername(string $username): ?User
     {
-        return $this->entityManager->getRepository(User::class)->findOneBy(['username' => $username]);
+        return $this->authRepository->findByUsername($username);
     }
 
     public function findById(int $id): ?User
     {
-        return $this->entityManager->getRepository(User::class)->find($id);
+        return $this->authRepository->find($id);
     }
 
     public function getUserFromSession(Request $request): ?User
@@ -73,3 +73,4 @@ class AuthService
         return $user;
     }
 }
+
